56dfd09b9223bcdee7539b294ddffda1d4466c6e
Harshit100306 <75255900+Harshit100306@users.noreply.github.com>
2021-03-27 09:01:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,11 +39,11 @@ function draw() {
   drawSprites();
 }
 
-function hasCollided(obj1, obj2) {
+function hasCollided(lbullet, lwall) {
 
-  obj1RightEdge = obj1.x + obj1.width;
-  obj2LeftEdge = obj2.x;
-  if (obj1RightEdge >= obj2LeftEdge) {
+  bulletRightEdge = lbullet.x + lwall.width;
+  wallLeftEdge = lwall.x;
+  if (bulletRightEdge >= wallLeftEdge) {
     return true;
 
   }
